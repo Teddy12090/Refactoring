cec8abf39e289ed25ce5af07fe78c151221a9464
Teddy Lee <teddy.cc.lee@linecorp.com>
2023-04-16 02:19:27 +0800

    delete variable

--- src/main/kotlin/refactoring/composingmethods/replacetempwithquery/Price.kt
+++ src/main/kotlin/refactoring/composingmethods/replacetempwithquery/Price.kt
@@ -2,7 +2,6 @@ package refactoring.composingmethods.replacetempwithquery
 
 class Price(private val quantity: Int, private val itemPrice: Double) {
     fun getPrice(): Double {
-        val basePrice = getBasePrice()
         val discountFactor = if (getBasePrice() > 1000) 0.95 else 0.98
         return getBasePrice() * discountFactor
     }
