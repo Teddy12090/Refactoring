3afee1c1e34b25df8c9ef07a212cf16f030e0fdb
Teddy Lee <teddy.cc.lee@linecorp.com>
2023-04-16 02:55:59 +0800

    declare variable

--- src/main/kotlin/refactoring/composingmethods/introduceexplainingvariable/Price.kt
+++ src/main/kotlin/refactoring/composingmethods/introduceexplainingvariable/Price.kt
@@ -7,6 +7,7 @@ class Price(private val itemPrice: Double, private val quantity: Int) {
     fun getPrice(): Double {
         val basePrice = quantity * itemPrice
         val quantityDiscount = max(0, quantity - 500) * itemPrice * 0.05
+        val shipping = min(basePrice * 0.1, 100.0)
         return basePrice - quantityDiscount + min(basePrice * 0.1, 100.0)
     }
 }
