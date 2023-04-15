cb6d256d9f897768ff1fb6bf73d889422023686d
Teddy Lee <teddy.cc.lee@linecorp.com>
2023-04-16 02:17:00 +0800

    replace code with the target function (compile, test)

--- src/main/kotlin/refactoring/composingmethods/replacetempwithquery/Price.kt
+++ src/main/kotlin/refactoring/composingmethods/replacetempwithquery/Price.kt
@@ -2,7 +2,7 @@ package refactoring.composingmethods.replacetempwithquery
 
 class Price(private val quantity: Int, private val itemPrice: Double) {
     fun getPrice(): Double {
-        val basePrice = quantity * itemPrice
+        val basePrice = getBasePrice()
         val discountFactor = if (basePrice > 1000) 0.95 else 0.98
         return basePrice * discountFactor
     }
