ccf70923da99af45768d8585bee34f429cda8b44
Teddy Lee <teddy.cc.lee@linecorp.com>
2023-04-16 02:52:46 +0800

    declare variable

--- src/main/kotlin/refactoring/composingmethods/introduceexplainingvariable/Price.kt
+++ src/main/kotlin/refactoring/composingmethods/introduceexplainingvariable/Price.kt
@@ -6,6 +6,7 @@ import kotlin.math.max
 class Price(private val itemPrice: Double, private val quantity: Int) {
     fun getPrice(): Double {
         val basePrice = quantity * itemPrice
+        val quantityDiscount = max(0, quantity - 500) * itemPrice * 0.05
         return basePrice - max(0, quantity - 500) * itemPrice * 0.05 + min(basePrice * 0.1, 100.0)
     }
 }
