913a5287dca6f7870d1fe9c652cabefd1cd3de2a
Teddy Lee <teddy.cc.lee@linecorp.com>
2023-04-16 02:18:12 +0800

    inline temp (compile, test)

--- src/main/kotlin/refactoring/composingmethods/replacetempwithquery/Price.kt
+++ src/main/kotlin/refactoring/composingmethods/replacetempwithquery/Price.kt
@@ -3,7 +3,7 @@ package refactoring.composingmethods.replacetempwithquery
 class Price(private val quantity: Int, private val itemPrice: Double) {
     fun getPrice(): Double {
         val basePrice = getBasePrice()
-        val discountFactor = if (basePrice > 1000) 0.95 else 0.98
+        val discountFactor = if (getBasePrice() > 1000) 0.95 else 0.98
         return basePrice * discountFactor
     }
 
