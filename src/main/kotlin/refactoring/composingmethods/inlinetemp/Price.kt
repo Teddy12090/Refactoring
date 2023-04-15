8d5ef5156c19619aae4ee13ccbd2bf2ea80f3eab
Teddy Lee <teddy.cc.lee@linecorp.com>
2023-04-16 01:42:40 +0800

    replace variable to code (compile, test)

--- src/main/kotlin/refactoring/composingmethods/inlinetemp/Price.kt
+++ src/main/kotlin/refactoring/composingmethods/inlinetemp/Price.kt
@@ -4,7 +4,7 @@ class Price(private val anOrder: Order) {
 
     fun basePriceOver1000(): Boolean {
         val basePrice = anOrder.basePrice
-        return basePrice > 1000
+        return anOrder.basePrice > 1000
     }
 }
 
