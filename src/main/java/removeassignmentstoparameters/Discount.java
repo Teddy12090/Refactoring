08f525a1f1883c3270e36986603be78cea900d5d
Teddy Lee <teddy.cc.lee@linecorp.com>
2023-04-16 18:24:23 +0800

    add final modifier to parameters

--- src/main/java/removeassignmentstoparameters/Discount.java
+++ src/main/java/removeassignmentstoparameters/Discount.java
@@ -2,7 +2,7 @@ package removeassignmentstoparameters;
 
 public class Discount {
     // In kotlin, there is no way to reassign value to parameter. In this case, java is used.
-    public int discount(int inputVal, int quantity, int yearToDate) {
+    public int discount(final int inputVal, final int quantity, final int yearToDate) {
         int result = inputVal;
         if (inputVal > 50) {
             inputVal -= 2;
